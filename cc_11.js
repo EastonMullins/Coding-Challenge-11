8dae3010b1ee5a65b4456ca4ab3b4cb0183faff5
Easton <eastonmullins@usf.edu>
2025-03-02 16:40:34 -0500

    Task 3 - Created Library Class

--- cc_11.js
+++ cc_11.js
@@ -55,3 +55,23 @@ console.log(borrower1.borrowedBooks);
 borrower1.returnBook("The Great Gatsby");
 console.log(borrower1.borrowedBooks);
 // Expected output: []
+
+
+// Task 3 - Created Library Class
+
+class Library{
+    constructor(books, borrowers){
+        this.books = [];
+        this.borrowers = [];
+    }
+    addBook(book) {
+        this.books.push(book);
+    }
+    listBooks() {
+        this.books.forEach(book => console.log(book.getDetails()));
+    }
+}
+const library = new Library();
+library.addBook(book1);
+library.listBooks();
+// Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
