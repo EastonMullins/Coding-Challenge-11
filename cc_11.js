21a87db2fded2e73ab368fc5430a2d45078f078b
Easton <eastonmullins@usf.edu>
2025-03-02 17:01:06 -0500

    Task 5 - Implemented Book Returns

--- cc_11.js
+++ cc_11.js
@@ -82,7 +82,17 @@ class Library{
             borrower.borrowBook(book.title);
         }
     }
+// Task 5 - Implemented Book Returns
 
+    returnBook(borrowerId, isbn) {
+        const book = this.books.find(b => b.isbn === isbn);
+        const borrower = this.borrowers.find(b => b.borrowerId === borrowerId);
+
+        if (book && borrower && borrower.borrowedBooks.includes(book.title)) {
+            book.updateCopies(1);
+            borrower.returnBook(book.title);
+        }
+    }
 }
 const library = new Library();
 library.addBook(book1);
@@ -95,4 +105,9 @@ console.log(book1.getDetails());
 console.log(borrower1.borrowedBooks);
 // Expected output: ["The Great Gatsby"]
 
+library.returnBook(201, 123456);
+console.log(book1.getDetails());
+// Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
+console.log(borrower1.borrowedBooks);
+// Expected output: []
 
